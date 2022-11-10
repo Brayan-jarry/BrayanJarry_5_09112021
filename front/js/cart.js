06c28403ca8187eb663b3a49bfef436d4b89be1e
Agathe INSEEC <agatheinseec@MacBook-Air-de-Agathe.local>
2022-11-10 15:07:32 +0100

    debut page panier

---
+++ front/js/cart.js
@@ -0,0 +1,23 @@
+fetch("http://localhost:3000/api/products")
+.then(response => response.json())
+.then(response => {
+    
+})
+
+let panier = localStorage.getItem("panier")
+
+function Vignettes_panier() {
+
+const article = document.createElement("article")
+const Div = document.createElement("div")
+const Img = document.createElement("img")
+const Kanap_name = document.createElement("h2")
+const Paragraphe = document.createElement("p")
+const Input = document.createElement("input")
+
+const Div_Img, Div_content, Div_content_description, Div_content_settings, Div_content_settings_quantity, Div_content_settings_delete = Div
+
+ Div_Img.classlist.add("cart_item_img")
+ Img.setAttribute("src",)
+
+}
--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,20 @@
 const queryString = window.location.search // on recupère les paramètre url
 const urlParams = new URLSearchParams(queryString) // on defini une variable pour aller chercher un paramètre en particulier
 const productId = urlParams.get('id') // on recupère l'id du produit
+
+// variable necéssaire pour le DOM
+let colors
+
+// variable necéssaire pour le panier
+let panier = []
+let couleur
 let detail_Produit
 let produits
 
 fetch('http://localhost:3000/api/products/' + productId)
 .then (response => response.json())
 .then (response => {
+    detail_Produit = response
     info_Produit(response)
     
 })
@@ -19,7 +27,7 @@ function info_Produit(params)
     const titre = document.getElementById("title")
     const prix = document.getElementById("price")
     const descriptions = document.getElementById("description")
-    const couleur = document.getElementById("colors")
+    couleur = document.getElementById("colors")
 
     // attribution des paramètres de chaque éléments
     titre.innerHTML = params.name
@@ -28,57 +36,76 @@ function info_Produit(params)
     image.setAttribute("src",params.imageUrl)
 
     // le paramètre couleur est un tableau on crée donc une boucle nous permettant de parcourir ces couleurs et de créer une option pour chacune d'entre elles
-    let colors =params.colors
+    colors =params.colors
     for (color of colors){
         const nouvelle_Couleur = document.createElement("option")
         nouvelle_Couleur.value = color
         nouvelle_Couleur.innerHTML = color
         couleur.appendChild(nouvelle_Couleur)
     }
-
-    detail_Produit = params
-    produits = {
-        produit_id : detail_Produit._id,
-        titre : detail_Produit.name,
-        prix : detail_Produit.price,
-        descrription : detail_Produit.description,
-        image : detail_Produit.imageUrl,
-        quantite : nb_Produit
-    }
 }
+
 // ajout des produits au panier
-
 const bouton_Panier = document.getElementById("addToCart")
-let nb_Produit= 1
-let produit_Dans_Panier = []
 
+bouton_Panier.addEventListener('click', function (){
 
+let nb_Produit= document.getElementById("quantity")
 
-bouton_Panier.addEventListener('click', function () {
-    console.log(localStorage.getItem('panier'))
-if (localStorage.getItem('panier')){
-    comparaison()
-}else{
-    ajout_produit()
+produits = {
+    produit_id : detail_Produit._id,
+    quantite : nb_Produit.value,
+    couleur : couleur.value
 }
-   
+
+
+if (localStorage.getItem('panier')){
+    comparaison(produits)
+}else{
+    ajout_produit(produits)
+}
+if(window.confirm('Le produit a été ajouté. Voulez-vous consulter votre panier ?')) {
+    window.location.replace("./cart.html")
+}
 } )
 
-function comparaison(){
-    let panier = JSON.parse(localStorage.getItem('panier'))
-    for (let i=0; i<=panier.length; i++){
-        console.log(panier[i].produit_id)
-        console.log(produits.produit_id)
-        if(panier[i].produit_id===produits.produit_id){
-            panier[i].quantite+=1
-        }else{
-            ajout_produit()
-        }
+function comparaison(produits){
+    panier = JSON.parse(localStorage.getItem('panier'))
+
+    // panier.forEach(function(index_panier){
+    //     if(index_panier.produit_id===produits.produit_id){
+
+    //         if (index_panier.couleur===produits.couleur){
+    //             index_panier.quantite = parseInt( index_panier.quantite )+ parseInt( produits.quantite)
+    //             console.log(panier)
+    //             localStorage.removeItem('panier')
+    //             console.log(localStorage.getItem('panier'))
+    //             localStorage.setItem('panier',JSON.stringify(panier))
+    //         }
+    //         else{
+    //             ajout_produit(produits)
+    //         }   
+    //     }
+    //     else{
+    //         ajout_produit(produits)
+    //     }
+    // })
+
+
+    let trouver_Produit = panier.find(p => p.produit_id === produits.produit_id && p.couleur === produits.couleur)
+
+    if(trouver_Produit){
+            trouver_Produit.quantite = parseInt( trouver_Produit.quantite )+ parseInt( produits.quantite)
+            localStorage.removeItem('panier')
+            localStorage.setItem('panier',JSON.stringify(panier))    
+    }
+    else{
+        ajout_produit(produits)
     }
 }
 
-function ajout_produit(){
-produit_Dans_Panier.push(produits)
-const produit_Dans_Panier_JSON = JSON.stringify(produit_Dans_Panier)
-localStorage.setItem('panier',produit_Dans_Panier_JSON)
-}
+function ajout_produit(produits){
+panier.push(produits)
+const Panier_JSON = JSON.stringify(panier)
+localStorage.setItem('panier',Panier_JSON)
+} 
